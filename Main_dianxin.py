049f974054440bd2a54dd4f6fe1ea25ba2f901ed
McEchean <983921194w@sina.com>
2018-04-26 18:16:14 +0800

    成功登陆

--- Main_dianxin.py
+++ Main_dianxin.py
@@ -9,8 +9,13 @@ import requests
 import json
 import execjs
 from PIL import Image
+import logging
+import re
+from http import cookiejar
 
 session = requests.session()
+session.cookies = cookiejar.LWPCookieJar('cookies.txt')
+logging.basicConfig(level=logging.INFO)
 
 Headers = {
     'Host': 'login.189.cn',
@@ -26,46 +31,80 @@ Headers = {
     'Accept-Language': 'zh-CN,zh;q=0.9',
 
 }
+Headers1 = {
+    'Host': 'login.189.cn',
+    'Connection': 'keep-alive',
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Origin': 'http://login.189.cn',
+    'X-Requested-With': 'XMLHttpRequest',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/65.0.3325.181 Safari/537.36',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'Referer': 'http://login.189.cn/web/login',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+
+}
+
 session.headers = Headers.copy()
 
 
-def _check_phone_num():
+def _get_init():
+    _get_init_url = 'http://login.189.cn/web/login'
+    session.headers.update({
+        'Upgrade-Insecure-Requests': '1',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    })
+    _get_init_resp = session.get(url=_get_init_url, headers=session.headers)
+    logging.info('_get_init_resp: %s' % _get_init_resp.status_code)
+    _get_init_url2 = 'http://www.189.cn/jt/loginpic/'
+    session.headers.update({
+       'Host': 'www.189.cn',
+    })
+    _get_init_resp2 = session.get(url=_get_init_url2, headers=session.headers)
+    logging.info('_get_init_resp2: %s' % _get_init_resp2.status_code)
+
+
+def _check_phone_num(phone_num):
     check_phonenum_url = 'http://login.189.cn/web/login/ajax'
     post_param = {
         'm': 'checkphone',
-        'phone': '17342071372',
+        'phone': phone_num,
     }
-    _check_resp = session.post(url=check_phonenum_url, data=post_param, headers=session.headers)
+    _check_resp = session.post(url=check_phonenum_url, data=post_param, headers=Headers)
+    logging.info('_check_resp: %s' % _check_resp.text)
     return json.loads(_check_resp.text)
 
 
-def _get_intface_captcha():
+def _get_intface_captcha(phone_num, pid):
     # session.headers.update({
     # })
     _get_captha_url = 'http://login.189.cn/web/login/ajax'
     post_param = {
         'm': 'captcha',
-        'account': '17342071372',
+        'account': phone_num,
         'uType': '201',
-        'ProvinceID': '12',
+        'ProvinceID': pid,
         'areaCode': None,
         'cityNo': None,
     }
-    _get_captha_resp = session.post(url=_get_captha_url, data=post_param, headers=session.headers)
+    _get_captha_resp = session.post(url=_get_captha_url, data=post_param, headers=Headers)
+    logging.info('_get_captha_resp: %s' % _get_captha_resp.text)
     return json.loads(_get_captha_resp.text)
 
 
-def _send_message():
+def _send_message(phone_num, pid):
     # session.headers.update({
     # })
     _send_message_url = 'http://login.189.cn/web/login/ajax'
     post_param = {
         'm': 'sendrandompwd',
-        'account': '17342071372',
+        'account': phone_num,
         'uType': '201',
-        'pid': '12',
+        'pid': pid,
     }
-    _send_message_resp = session.post(url=_send_message_url, data=post_param, headers=session.headers)
+    _send_message_resp = session.post(url=_send_message_url, data=post_param, headers=Headers)
+    logging.info('_send_message_resp: %s' % _send_message_resp.text)
     return json.loads(_send_message_resp.text)
 
 
@@ -82,23 +121,51 @@ def _get_captcha_img():
     img.show()
 
 
-def _login(phone_num, password, captcha_num):
+def _check_pwd_valdate(phonw_number, password):
+    _check_pwd_url = 'http://login.189.cn/web/pwd/validate'
+    post_param = {
+        'uName': phonw_number,
+        'uType': '201',
+        'uPwd': password,
+        'isRandomPwd': 'true',
+    }
+    _check_pwd_resp = session.post(url=_check_pwd_url, data=post_param, headers=Headers)
+    logging.info('_check_pwd_resp: %s' % _check_pwd_resp.text)
+
+
+def _login(phone_num, password, captcha_num, pid):
     session.headers.update({
-        'Cache-Control': 'max-age=0',
         'Upgrade-Insecure-Requests': '1',
+        'Content-Type': 'application/x-www-form-urlencoded',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
     })
     _login_url = 'http://login.189.cn/web/login'
     post_param = {
         'Account': phone_num,
         'UType': '201',
-        'ProvinceID': '12',
+        'ProvinceID': pid,
         'AreaCode': None,
         'CityNo': None,
         'RandomFlag': '1',
         'Password': _get_pwd(password),
         'Captcha': captcha_num,
     }
+    _login_resp = session.post(url=_login_url, data=post_param, headers=session.headers, allow_redirects=True)
+    logging.info('_login_resp: %s' % _login_resp.text)
+
+    # match_http = re.match(r'.*?PlatNO=(\d+).*?ResultCode=(\d+).*?Ticket=(\w+).*?TxID=(\w+).*',
+    #                       _login_resp.text, re.DOTALL).group(1, 2, 3, 4)
+    # match_http = _login_resp.headers.get('Location', '')
+    # return match_http
+
+
+def _get_ecsdo(match_http):
+    session.headers.update({
+        'Host': 'www.189.cn',
+    })
+    _get_ecsdo_url = 'http://www.189.cn/login/ecs.do?PlatNO={0}&ResultCode={1}&Ticket={2}&TxID={3}'.format(*match_http)
+    _get_ecsdo_resp = session.get(url=_get_ecsdo_url, headers=session.headers)
+    logging.info('_get_ecsdo_resp: %s' % _get_ecsdo_resp.status_code)
 
 
 def _get_js():
@@ -116,7 +183,7 @@ def _get_pwd(pwd):
     ctx = execjs.compile(jsstr)
     return ctx.call('valAesEncryptSet', pwd)
 
-
+# 10036201804261110109dc31dd30fd04917cadcb0c1442d8f14
 # def get_runntime(path):
 #     """
 #     :param path: 加密js的路径,注意js中不要使用中文！估计是pyexecjs处理中文还有一些问题
@@ -136,4 +203,20 @@ def _get_pwd(pwd):
 # print(_get_intface_captcha())
 # # print(_send_message())
 # _get_captcha_img()
-# print(_get_pwd('123456'))
+# print(_get_pwd('564184'))
+
+
+_get_init()
+phone_num = input('请输入手机号：')
+info = _check_phone_num(phone_num)
+pid = info['provinceId']
+_get_intface_captcha(phone_num, pid)
+_get_captcha_img()
+captcha_num = input('请输入图片验证码:')
+_get_intface_captcha(phone_num, pid)
+_send_message(phone_num, pid)
+_get_intface_captcha(phone_num, pid)
+password = input('请输入手机验证码：')
+_check_pwd_valdate(phone_num, password)
+_login(phone_num, password, captcha_num, pid)
+# _get_ecsdo(match_http)
--- test.py
+++ test.py
@@ -5,8 +5,85 @@
 # @File    : test.py
 # @Software: PyCharm
 
-import base64
+# import base64
+# #
+# #
+# # s = b'123456'
+# # print(base64.b64encode(s))
+
+# from scrapy import Selector
+#
+# doc = ''
+# sel = Selector()
+# sel.css(login_response,)
 
 
-s = b'123456'
-print(base64.b64encode(s))
+# import requests
+# from scrapy import Selector
+#
+# Headers = {
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)'
+#                   ' Chrome/65.0.3325.181 Safari/537.36'
+# }
+#
+# url = 'https://www.baidu.com/'
+# session = requests.session()
+#
+# resp = session.get(url,headers=Headers)
+# sel = Selector(resp)
+# print(sel.css('script::text').extract_first(''))
+
+
+import re
+_login_resp = '''
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<HTML>
+<HEAD>
+    <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=gbk">
+    <TITLE>´íÎó£ºÄúËùÇëÇóµÄÍøÖ·£¨URL£©ÎÞ·¨»ñÈ¡</TITLE>
+    <STYLE type="text/css"><!--
+    BODY {
+        background-color: #ffffff;
+        font-family: verdana, sans-serif
+    }
+
+    PRE {
+        font-family: sans-serif
+    }
+
+    --></STYLE>
+</HEAD>
+<BODY>
+<H1>´íÎó</H1>
+<H2>ÄúËùÇëÇóµÄÍøÖ·£¨URL£©ÎÞ·¨»ñÈ¡</H2>
+<HR noshade size="1px">
+<P>
+    µ±³¢ÊÔ¶ÁÈ¡ÒÔÏÂÍøÖ·£¨URL£©Ê±£º
+    <A HREF="http://login.189.cn/login/ecs.do?PlatNO=90000&amp;ResultCode=0&amp;Ticket=900000035138b169f9b8498242b598fd256b1b446540&amp;TxID=10036900006420180426120034000012">http://login.189.cn/login/ecs.do?PlatNO=90000&amp;ResultCode=0&amp;Ticket=900000035138b169f9b8498242b598fd256b1b446540&amp;TxID=10036900006420180426120034000012</A>
+<P>
+    ·¢ÉúÁËÏÂÁÐµÄ´íÎó£º
+    <UL>
+        <LI>
+            <STRONG>
+                Access Denied.
+                <BR>¾Ü¾ø·ÃÎÊ
+            </STRONG>
+<P>
+    Access control configuration prevents your request from
+    being allowed at this time. Please contact your service provider if
+    you feel this is incorrect.
+    <BR>
+    µ±Ç°µÄ´æÈ¡¿ØÖÆÉè¶¨½ûÖ¹ÄúµÄÇëÇó±»½ÓÊÜ£¬
+    Èç¹ûÄú¾õµÃÕâÊÇ´íÎóµÄ£¬ÇëÓëÄúÍøÂ··þÎñµÄÌá¹©ÕßÁªÏµ¡£
+    </UL>
+</P>
+<P>±¾»º´æ·þÎñÆ÷¹ÜÀíÔ±£º<A HREF="mailto:support@chinacache.com">support@chinacache.com</A>
+'''
+# match_http = re.search(r'^>http://[^;]+<$', _login_resp, re.S)
+match_http = re.findall(r'>http://.*?<', _login_resp, re.DOTALL)
+print(match_http)
+match_http = re.findall(r'PlatNO=\d{4}?', _login_resp, re.DOTALL)
+match_http = re.match(r'.*?PlatNO=(\d+).*?ResultCode=(\d+).*?Ticket=(\w+).*?TxID=(\w+).*', _login_resp, re.DOTALL).group(1,2,3,4)
+
+
+print(match_http)
